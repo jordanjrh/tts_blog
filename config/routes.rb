e988f969d5ff63e85309994c93968768baedc496
Jordan Herrera <jordanherrerajrh@gmail.com>
2017-08-17 09:36:57 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  resources :posts
+root 'practice#index'
+
+get 'index' => 'practice#index'
+
+get 'posts' => 'post#index'
+
+get 'about' => 'practice#about'
+
+
+
+
+  # root 'practice#index'
+  # get 'index' => 'practice#index'
+  # post 'index' => 'practice#index'
+  # get 'about' => 'practice#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
